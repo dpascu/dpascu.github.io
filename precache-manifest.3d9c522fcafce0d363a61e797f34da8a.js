89974f2d9fdffa94b58ab882a97a7cb2af70b37e
dpascu <dany3pp@gmail.com>
2020-11-01 03:11:37 +0100

    initial commit

---
+++ precache-manifest.3d9c522fcafce0d363a61e797f34da8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d678767c6df057f623b8a602e49a48f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "987cb5af704b937fbe69",
+    "url": "/static/css/main.f435ebd4.chunk.css"
+  },
+  {
+    "revision": "3e2e744027f93f787cad",
+    "url": "/static/js/2.dab178cb.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.dab178cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "987cb5af704b937fbe69",
+    "url": "/static/js/main.d13625e1.chunk.js"
+  },
+  {
+    "revision": "795cc0970545dbb75983",
+    "url": "/static/js/runtime-main.254e5265.js"
+  }
+]);
